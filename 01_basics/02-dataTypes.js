fa8e883501058d00308b03e862c05fa72e92a7d0
Bishwarup Biswas <bishwarupbiswas07@gmail.com>
2025-08-24 11:51:20 +0000

    datatypes

---
+++ 01_basics/02-dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+// alert(6+6)  // we are using node.js, not browser
+
+
+console.log(3
+    +
+    3)   // code readability should be high
+
+
+console.log("dipto")
+
+let name = "dipto"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+
+//null => standalone value
+//undefined => 
+// symbol => uniqe
+//object 
+
+console.log(typeof "dipto");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
