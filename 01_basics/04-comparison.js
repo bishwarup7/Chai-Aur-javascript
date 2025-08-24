03ae4c0dcd02cf44eaa5efe8f7b0834055d2f63a
Bishwarup Biswas <bishwarupbiswas07@gmail.com>
2025-08-24 13:56:12 +0000

    Comparison

---
+++ 01_basics/04-comparison.js
@@ -0,0 +1,31 @@
+
+
+// console.log(5 > 1);
+// console.log(5 >= 1);
+// console.log(5 < 1);
+// console.log(5 == 1);
+// console.log(5 != 1);
+
+// console.log("5" > 1);
+// console.log("5" > 1);
+
+
+console.log(null >0);
+console.log(null ==0);
+console.log(null >=0);
+
+
+console.log(undefined >0);
+console.log(undefined ==0);
+console.log(undefined <0);
+
+
+
+
+// ===
+console.log("6" === 2);
+
+
+
+
+
