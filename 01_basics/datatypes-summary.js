e7b009696ebf3f89a53e473bc008e56a80c69ad4
Bishwarup Biswas <bishwarupbiswas07@gmail.com>
2025-08-25 15:22:27 +0000

    Added primitive datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// primitive 
+
+// types : Strin,. Number, Boolean,  "null, undefine", Symbol,BigInt
+
+const score = 100
+
+const scoreValue = 100
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+
+const anotherId = Symbol("123")
+console.log(id === anotherId);
+const bigNumber = 325647895478n
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const usa =["New jersey", "Florida","Naveda"]
+
+let = {
+    name: "Bishwarup",
+    Age : 19
+}
+
+const myfunction = function(){
+    console.log(typeof scoreValue);
+    
+}
+
+
+
+
+
+
+
+
+
