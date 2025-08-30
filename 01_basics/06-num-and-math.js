05a72b752a1a4874a5e1b38092c3a9679b2b75da
Bishwarup Biswas <bishwarupbiswas07@gmail.com>
2025-08-30 09:08:14 +0000

    numbers&maths

---
+++ 01_basics/06-num-and-math.js
@@ -0,0 +1,38 @@
+const score = 400
+//console.log(score)
+
+const balance = new Number(100)
+//console.log(balance);
+
+
+//console.log(balance.toString().length);
+
+//console.log(balance.toFixed(2));
+
+const ot = 22.36548
+//console.log(ot.toPrecision(3));
+
+const hund = 1000000
+//console.log(hund.toLocaleString());
+//console.log((hund.toLocaleString("en-IN")));
+
+//------------------maths-----------------------------
+
+console.log(Math);
+console.log(Math.round(4.6));
+console.log(Math.abs(-4));  //only giving positive value
+console.log(Math.ceil(4.2)); //upper value
+console.log(Math.floor(4.9)); //lower value
+console.log(Math.max(4,5,6,9)); //  max value
+console.log(Math.min(4,5,6,9)); // min value
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =20
+const max = 40
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
